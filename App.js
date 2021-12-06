c54dfe183bf811e0be48075373d5c26c1e1031fc
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2021-12-06 17:01:42 -0300

    hello world

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-rn'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw("flex-1 justify-center items-center bg-yellow-400")}>
+      <Text>Oi mundo!</Text>
       <StatusBar style="auto" />
     </View>
   );
