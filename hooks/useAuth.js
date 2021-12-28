2cd9c56739f48f64dc72dcdc146fe67bc69bd661
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2021-12-28 20:52:12 -0300

    setting firebase :gear:

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,11 +1,17 @@
 import React, { createContext, useContext } from 'react';
 import { View } from 'react-native';
+import * as Google from 'expo-google-app-auth';
 
 const AuthContext = createContext({
 
 })
 
 export const AuthProvider = ({ children }) => {
+
+    const signInWithGoogle = async () => {
+        await Google.signInAsync()
+    }
+
     return (
         <AuthContext.Provider value={{ user: null, }}>
             {children}
