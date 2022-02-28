bafcbc5ca4721c4dc7b79f03e692183699ebf096
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2022-02-28 17:54:03 -0300

    swiper done :art:

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, Button, TouchableOpacity, Image } from 'react-native';
+import { View, Text, Button, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'tailwind-rn';
 import { Ionicons } from '@expo/vector-icons';
@@ -66,9 +66,51 @@ const HomeScreen = () => {
         <Swiper
           containerStyle={{ backgroundColor: 'transparent' }}
           cards={DUMMY_DATA}
+          stackSize={5}
+          cardIndex={0}
+          verticalSwipe={false}
+          animateCardOpacity
+          overlayLabels={{
+            left: {
+              title: "NOPE",
+              style: {
+                label: {
+                  textAlign: "right",
+                  color: "red",
+                }
+              }
+            },
+            right: {
+              title: "MATCH",
+              style: {
+                label: {
+                  color: "#4DED30",
+                }
+              }
+            },
+          }}
           renderCard={(card) => (
-            <View key={card.id} style={tw("bg-red-500 h-3/4 rounded-xl")}>
-              <Text>{card.firstName}</Text>
+            <View
+              key={card.id}
+              style={tw("relative bg-white h-3/4 rounded-xl")}
+            >
+              <Image
+                style={tw('absolute top-0 h-full w-full rounded-xl')}
+                source={{ uri: card.photoURL }}
+              />
+              <View
+                style={[tw(
+                  'absolute bottom-0 bg-white w-full flex-row justify-between items-between h-20 px-6 py-2 rounded-b-xl'
+                ), style.cardShadow
+                ]}>
+                <View>
+                  <Text style={tw("text-xl font-bold")} >
+                    {card.firstName} {card.lastName}
+                  </Text>
+                  <Text>{card.occupation}</Text>
+                </View>
+                <Text style={tw("text-2xl font-bold")}>{card.age}</Text>
+              </View>
             </View>
           )}
         />
@@ -79,3 +121,17 @@ const HomeScreen = () => {
 }
 
 export default HomeScreen;
+
+const style = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+});
