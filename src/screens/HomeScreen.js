66f5e1fae14e97ed0c691bdfc86e25c7729119a6
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2022-04-26 20:49:56 -0300

    swipes working ✨

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,48 +2,18 @@ import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { View, Text, Button, TouchableOpacity, Image, StyleSheet, LogBox } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import tw from 'tailwind-rn';
 import Swiper from 'react-native-deck-swiper'
-import { Ionicons, Entypo, AntDesign } from '@expo/vector-icons';
-
 import useAuth from '../../hooks/useAuth'
-import { collection, doc, getDocs, onSnapshot, query, setDoc, where } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, onSnapshot, query, setDoc, where } from 'firebase/firestore';
 import { db } from '../../hooks/firebase';
-
-const DUMMY_DATA = [
-  {
-    firstName: "Juan",
-    lastName: "Andrade",
-    occupation: 'Dev',
-    photoURL: "https://pbs.twimg.com/profile_images/1428180830271201285/_KOGlUz7_400x400.jpg",
-    age: 19,
-    id: 1,
-  },
-  {
-    firstName: "Sabrina",
-    lastName: "Kudo",
-    occupation: 'Moda',
-    photoURL: "https://pbs.twimg.com/media/FK8gbcsWYAQUAAb?format=jpg&name=small",
-    age: 20,
-    id: 2,
-  },
-  {
-    firstName: "Pedro",
-    lastName: "Monteiro",
-    occupation: 'História',
-    photoURL: "https://instagram.fdiq5-1.fna.fbcdn.net/v/t51.2885-19/218736818_1626782224190839_5959746500303627566_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fdiq5-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Ujh_ULKIr28AX_r59Jy&edm=ABfd0MgBAAAA&ccb=7-4&oh=00_AT_HQZq1pFvIE_rg5KX4QqYbOvfkWo9gudhHcujvEzqstw&oe=62251633&_nc_sid=7bff83",
-    age: 20,
-    id: 2,
-  },
-]
+import { Ionicons, Entypo, AntDesign } from '@expo/vector-icons';
+import tw from 'tailwind-rn';
 
 const HomeScreen = () => {
 
   // um useEffect de quando o componente aparece na tela
   useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown: false,
-    });
+    navigation.setOptions({ headerShown: false, });
   }, []);
 
   const navigation = useNavigation();
@@ -68,45 +38,57 @@ const HomeScreen = () => {
     let unsub;
 
     const fetchCards = async () => {
-      const passes = await getDocs(collection(db, "users", user.uid, "passes"))
-        .then((snapshot) => snapshot.docs.map((doc) => doc.id)
-        );
+      const passes = await getDocs(collection(db, 'users', user.uid, 'passes'))
+      .then(snapshot => snapshot.docs.map((doc) => doc.id))
+
       const swipes = await getDocs(collection(db, "users", user.uid, "swipes"))
         .then((snapshot) => snapshot.docs.map((doc) => doc.id)
         );
 
+        
+        // console.log(passedUserIds,"1", swipedUserIds, "2")
+      };
       const passedUserIds = passes.length > 0 ? passes : ['test'];
       const swipedUserIds = swipes.length > 0 ? swipes : ['test'];
-      // console.log(passedUserIds,"1", swipedUserIds, "2")
-      unsub = onSnapshot(
-        query(
-          collection(db, "users"),
-          where("id", "not-in", [...passedUserIds, ...swipedUserIds])), (snapshot) => {
-            setProfiles(
-              snapshot.docs.filter(doc => doc.id !== user.uid).map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-              }))
-            );
-          });
-    };
+
+    unsub = onSnapshot(
+      query(
+        collection(db, "users"),
+        where("id", "not-in", [...passedUserIds, ...swipedUserIds])), 
+        (snapshot) => {
+          setProfiles(
+            snapshot.docs.filter(doc => doc.id !== user.uid).map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
 
     fetchCards();
     return unsub;
   }, [db]);
 
-  const swipeLeft = async (cardIndex) => {
+  const swipeLeft = (cardIndex) => {
+    // se não tiver esse profile ele só retorna
     if (!profiles[cardIndex]) return
 
     const userSwiped = profiles[cardIndex];
     console.log(`${user.displayName} passou ${userSwiped.displayName}`)
 
-    // ele coloca no db o id do usuario que foi swipado
+    // ele coloca no db o id do usuario que foi swipado em uma collection passes
     setDoc(doc(db, 'users', user.uid, 'passes', userSwiped.id),
       userSwiped);
   }
-  const swipeRight = async () => {
 
+  const swipeRight = async (cardIndex) => {
+    if (!profiles[cardIndex]) return
+
+    const userSwiped = profiles[cardIndex];
+    console.log(`${user.displayName} quer match com ${userSwiped.displayName}`)
+
+
+    setDoc(doc(db, 'users', user.uid, 'swipes', userSwiped.id),
+      userSwiped);
   }
   // console.log(profiles);
   return (
